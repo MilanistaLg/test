8cd6b3e3b3636d0158c5f631b3033b1ee5faa00c
Lemish Andrey <lemishandrey@gmail.com>
2020-04-20 18:00:16 +0300

    commit #4, lesson#1 done

---
+++ lesson 1/js.js
@@ -0,0 +1,19 @@
+'Use strict';
+let money = +prompt ("Ваш бюджет на месяц?", "");
+let time = +prompt ("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
+        a : b
+    },
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+var a = 
++prompt ("Введите обязательную статью расходов в этом месяце", "");
+var b = 
++prompt ("Во сколько обойдется?", "");
+alert(appData["budget"]/30);
